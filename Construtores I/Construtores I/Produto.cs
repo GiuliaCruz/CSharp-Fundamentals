71df974af96994ab14dc72b8ab1ef41aef0db814
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 16:02:48 -0300

    Learn about constructors

---
+++ Construtores I/Construtores I/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores_I {
+    class Produto {
+        public string Nome;
+        public double Preço;
+        public int Quantidade; 
+
+        public double ValorTotalEmEstoque() {
+            return Preço * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+        public override string ToString() {
+            return Nome
+                + ", $ "
+                + Preço.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+             
+        }
+    }
+}
