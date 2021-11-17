35a12585d82c1fb58c8e31d394497ff0647d3826
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 16:16:14 -0300

    Learn about insert data

---
+++ Entrada de dados 2.0/Entrada de dados 2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entrada_de_dados_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+        }
+    }
+}
