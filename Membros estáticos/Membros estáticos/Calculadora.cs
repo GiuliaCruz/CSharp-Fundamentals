37ac441ce6a11c1e075cd9abed7d608fe14593cd
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 16:04:02 -0300

    Learn about static members

---
+++ Membros estáticos/Membros estáticos/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Membros_estáticos {
+    class Calculadora {
+         public static double Pi = 3.14;
+
+         public static double Circunferencia(double raio) {
+            return 2.0 * Pi * raio;
+        }
+
+         public static double Volume(double raio) {
+            return 4.0 / 3.0 * Pi * raio * raio * raio;
+        }
+    }
+}
