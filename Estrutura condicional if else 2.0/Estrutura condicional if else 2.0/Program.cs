17c4ae8111825a64ca358ef082106832968db70c
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 16:07:06 -0300

    Learn about if else

---
+++ Estrutura condicional if else 2.0/Estrutura condicional if else 2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Estrutura_condicional_if_else_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("bom dia");
+            }
+            else if (hora < 18) {
+                Console.WriteLine("Boa tarde");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite");
+            }
+        }
+    }
+}
