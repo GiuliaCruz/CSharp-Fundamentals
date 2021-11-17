9fde1be97439693438772d059434f1a3b557c3f4
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 16:05:18 -0300

    Learn about while statement

---
+++ Estrutura while/Estrutura while/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estrutura_while {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um número:");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
