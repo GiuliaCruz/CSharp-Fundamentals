118dedc1676587d9c7c9b14f217d14a2a841bb30
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 15:59:04 -0300

    Learn about Params

---
+++ Params/Params/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Params {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = Calculator.Sum( 2, 3 );
+            int x2 = Calculator.Sum( 2, 4, 3 );
+
+            Console.WriteLine(x);
+            Console.WriteLine(x2);  
+        }
+    }
+}
