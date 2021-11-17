861566f70f76c5b65fb385a67d90d88f4fcc06a3
GiuliaCruz <giuliacruzz@gmail.com>
2021-11-17 15:58:32 -0300

    Learn about Params

---
+++ Params ref e out/Params ref e out/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Params_ref_e_out {
+    class Calculator {
+        /*ref
+        public static void Triple(ref int x) {
+            x = x * 3;
+        }
+        */
+
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Params ref e out/Params ref e out/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Params_ref_e_out {
+    class Program {
+        static void Main(string[] args) {
+            /* ref
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+            */ 
+
+            /*out*/  
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
