44466e8a22e2db59bec5be6258b7703bd2a7e7d5
GiuliaCruz <giuliacruzz@gmail.com>
2021-12-01 16:21:58 -0300

    Learning about Ternary operator

---
+++ Ternary operator/Ternary operator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+namespace Ternary_operator {
+    class Program {
+        static void Main(string[] args) {
+
+            double preço = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preço < 20.0) ? preço * 0.1 : preço * 0.05;
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
